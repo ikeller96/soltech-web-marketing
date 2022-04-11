2cfbcf7bcaf63f6675a8267027611e1c2433e911
Ian Keller <ianwkeller@gmail.com>
2022-04-11 11:17:12 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./images/Logo.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Improved All-In-One Solar Software Coming Soon</h3>
+        <div className="Link">
+          <p className="SmallText">
+            Interested in Solar?&nbsp;
+          </p>
+            <a
+              className="App-link"
+              href="https://goionixsolar.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Contact Ionix!
+            </a>
+        </div>
       </header>
     </div>
   );
